e91533ef563adc38abc46ce781d1fd912108ed9a
blck-snwmn <whitesnowmancreator@gmail.com>
2022-06-06 02:35:55 +0900

    Add slice benchmark

---
+++ slice/slice.go
@@ -0,0 +1,20 @@
+package slice
+
+//go:noinline
+func genSlice(b []byte) {
+	b[0] = 0x10
+	b[1] = 0x11
+	b[2] = 0x12
+}
+
+//go:noinline
+func returnGenSlice() []byte {
+	return []byte{0x10, 0x11, 0x12}
+}
+
+var dest = []byte{0x10, 0x11, 0x12}
+
+//go:noinline
+func returnSlice() []byte {
+	return dest
+}
---
+++ slice/slice_test.go
@@ -0,0 +1,30 @@
+package slice
+
+import (
+	"testing"
+)
+
+func Benchmark_genSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		dest := make([]byte, 3)
+		genSlice(dest)
+		none(dest)
+	}
+}
+
+func Benchmark_returnGenSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		dest := returnGenSlice()
+		none(dest)
+	}
+}
+
+func Benchmark_returnSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		dest := returnSlice()
+		none(dest)
+	}
+}
+
+// 確保した変数を消費するだけの関数.
+func none([]byte) {}
